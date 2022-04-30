01dad64cee15e8aed7ad967462b41d534888013f
Donovan <Donovan1478@yahoo.com>
2022-04-30 12:22:51 -0500

    edited service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -28,7 +28,7 @@ const FILES_TO_CACHE = [
       caches.open(CACHE_NAME).then(cache => {
         console.log('Your files were pre-cached successfully!');
         return cache.addAll(FILES_TO_CACHE);
-      })
+      }).catch(error => {console.error(error)})
     );
     self.skipWaiting();
   });
